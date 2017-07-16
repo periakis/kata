c41c39fb65391d3939eb8a11778549b6f2083064
Dong <Dong@61.77.116.195>
2017-07-16 18:56:18 +0900

    isFriendsWith(user) method is added to User

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,33 @@
 package org.craftedsw.tripservicekata.user;
 
-public class UserTest {
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
 
+import org.junit.Test;
+
+public class UserTest {
+	
+	private static final User BOB = new User();
+	private static final User PAUL = new User();
+
+	@Test
+	public void should_inform_when_users_are_not_friends() throws Exception {
+		
+		User user = UserBuilder.aUser()
+						.friendsWith(BOB)
+						.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(false));
+	}
+	
+	@Test
+	public void should_inform_when_uesrs_are_friends() throws Exception {
+
+		User user = UserBuilder.aUser()
+				.friendsWith(BOB, PAUL)
+				.build();
+		
+		assertThat(user.isFriendsWith(PAUL), is(true));
+		
+	}
 }
