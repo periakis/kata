47a9e3b6a798e79c8cda883f8bafbab9d9cb23c9
Dong <Dong@61.77.116.195>
2017-07-16 19:25:26 +0900

    TripService refectored

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,32 +10,22 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
+		if (getLoggedInUser() == null) {
 			throw new UserNotLoggedInException();
 		}
+
+		return user.isFriendsWith(getLoggedInUser()) ? tripsBy(user) : noTrips();
 	}
-	
-	
-	
+
 	protected User getLoggedInUser() {
 		return UserSession.getInstance().getLoggedUser();
 	}
-	
-	protected List<Trip> tripsBy(User user){
+
+	protected List<Trip> tripsBy(User user) {
 		return TripDAO.findTripsByUser(user);
+	}
+	
+	private ArrayList<Trip> noTrips() {
+		return new ArrayList<Trip>();
 	}
 }
